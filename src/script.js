cb27e06461f1817c919fba9851dbbca178d3e776
Jeffste <contact@jeffvansteijn.nl>
2022-01-27 21:14:27 +0100

    scroll

--- src/script.js
+++ src/script.js
@@ -9,4 +9,11 @@ var myScrollFunc = function () {
     }
 };
 
-window.addEventListener("scroll", myScrollFunc);
+window.onscroll = function() {progressIndicator()};
+
+function progressIndicator() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
