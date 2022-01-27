1418d81662f5906a482813f7cad17314ae56dfad
Jeffste <contact@jeffvansteijn.nl>
2022-01-27 21:19:38 +0100

    remove scroll bar

--- src/script.js
+++ src/script.js
@@ -9,11 +9,4 @@ var myScrollFunc = function () {
     }
 };
 
-window.onscroll = function() {progressIndicator()};
-
-function progressIndicator() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("progressBar").style.width = scrolled + "%";
-}
+window.addEventListener("scroll", myScrollFunc);
